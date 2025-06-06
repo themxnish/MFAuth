45888ae26fd499df146c9af43654aa794a675619
themxnish <ohhmxnish@gmail.com>
2025-06-06 12:45:38 +0530

    create UI for signup page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ export const metadata = {
   description: '',
 };
 
+import './globals.css';
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
