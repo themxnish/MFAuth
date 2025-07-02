1a2a806b9cf6cf7aae7019ba92a68d8dd98d4169
themxnish <ohhmxnish@gmail.com>
2025-07-02 16:04:03 +0530

    add complete OTP verification UI

---
+++ src/app/verify-email/page.tsx
@@ -0,0 +1,72 @@
+'use client';
+
+import { useState } from 'react';
+import { MailOpen, KeySquare, MoveRight, CheckCircle, CheckCheck } from 'lucide-react';
+import { redirect } from 'next/navigation';
+
+export default function VerifyEmail() {
+  const [ email, setEmail ] = useState('');
+  const [ page, setPage ] = useState<'email' | 'otp' | 'verified'>('email');
+  
+  const [ otpInput, setOtpInput ] = useState(['', '', '', '']);
+
+  const handleOtpChange = (index: number, value: string) => {
+    if (!/^\d?$/.test(value)) return;// allow only numbers
+
+    const newOtpInput = [...otpInput];
+    newOtpInput[index] = value;
+    setOtpInput(newOtpInput);
+  }
+
+  const verified = async () => {
+    redirect('/');
+  }
+
+  return (
+    <div className='min-h-screen flex flex-col items-center justify-center px-4'>
+      <div className='max-w-md w-full px-6 py-10 bg-[#3B3B3C] text-white shadow-xl rounded-xl'>
+        <h1 className='text-2xl font-bold text-center'>Email Verification</h1>
+        <p className='text-sm text-gray-400 mb-4 text-center'>Confirm your identity for <strong>secured access</strong>.</p>
+        
+        {page === 'email' &&
+          <div className='flex flex-col items-center justify-center gap-4'>
+            <MailOpen className='w-16 h-16 text-gray-300 mt-6' />
+            <p className='text-md text-gray-300'>Please enter your email address to receive a 4-digit code</p>
+            <input className='text-white text-md w-full px-4 py-3 bg-[#4B4B4B] shadow-xl rounded-lg' type='email' name='email' placeholder='abc@gmail.com' value={email} onChange={(e) => setEmail(e.target.value)} />
+            <button className='bg-[#4B4B4B] text-white w-1/2 py-3 px-4 rounded-full shadow-xl mt-4 hover:scale-105 cursor-pointer'>
+              <MoveRight className='w-6 h-6 inline-block' />
+            </button>
+          </div>
+        }
+
+        {page === 'otp' &&
+          <div className='flex flex-col items-center justify-center gap-4'>
+            <KeySquare className='w-16 h-16 text-yellow-300 mt-6'/>
+            <div className='text-center'>
+              <p className='text-md text-gray-300'>Code has been sent to:</p>
+              <strong className='text-md text-white'>{email}</strong>
+            </div>
+            <div className='flex gap-4'>
+              {otpInput.map((value, index) => (
+                <input key={index} className='text-center font-bold text-white text-xl w-12 px-4 py-3 bg-[#4B4B4B] shadow-xl rounded-lg' type='text' inputMode='numeric' maxLength={1} value={value} onChange={(e) => handleOtpChange(index, e.target.value)} />
+              ))}
+            </div>
+            <p className='text-sm text-gray-400'>The OTP expires in next 3 minutes</p>
+            <button className='bg-[#4B4B4B] text-white font-semibold w-1/2 py-3 px-4 rounded-full shadow-xl mt-6 hover:scale-105 cursor-pointer'>Verify</button>
+            <p className='text-sm text-gray-400'>Did not receive the code? <span className='text-yellow-300 cursor-pointer' onClick={() => { setPage('email'); setOtpInput(['', '', '', ''])}}>Resend</span></p>
+          </div>
+        }
+
+        {page === 'verified' && 
+          <div className='flex flex-col items-center justify-center gap-4'>
+            <CheckCircle className='w-16 h-16 text-green-400 mt-6' />
+            <p className='text-md text-gray-300'>Your email has been verified successfully</p>
+            <button className='bg-[#4B4B4B] text-sky-400 w-1/2 py-3 px-4 rounded-full shadow-xl mt-6 hover:scale-105 cursor-pointer' onClick={verified}>
+              <CheckCheck className='w-6 h-6 inline-block' />
+            </button>
+          </div>
+        }
+      </div>
+    </div>
+  );
+}
